f2f728924780c449aee229e34aa0e057e507af23
Ashley Maceli and Nancy Fang <ashley@>
2016-03-22 11:10:45 -0700

    Change colors on page using JS

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+  $("button#blueB").click(function() {
+    $("body").removeClass();
+    $("body").addClass("blue-background");
+  });
+
+  $("button#greenB").click(function() {
+    $("body").removeClass();
+    $("body").addClass("green-background");
+  });
+
+  $("button#orangeB").click(function() {
+    $("body").removeClass();
+    $("body").addClass("orange-background");
+  });
+
+
+});
